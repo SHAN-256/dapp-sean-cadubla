b57dafa5da294c25b90a1e97982bb1aebe98cde9
Sean Cadubla <seanrc323@gmail.com>
2018-09-28 22:01:14 +0800

    Final touches to the program. Alerts with Bootstrap Notify, Uploading images to IPFS, Comments and general tidying up.

--- contracts/mtgMarket1.sol
+++ contracts/mtgMarket1.sol
@@ -7,9 +7,9 @@ import "./safemath.sol";
 contract mtgInventory {
 // @notice using SafeMath from safemath.sol for uint256
     using SafeMath for uint256;
-    
+// @notice used to check if a newcard is added
     event NewCardInventory(uint cardId, string cardName, uint cardPrice);
-    
+// @notice struct for a mtgCard. 
     struct mtgCard {
         uint id;
         string name;
@@ -19,12 +19,21 @@ contract mtgInventory {
         uint price;
         string image;
     }
-    
+// @notice making an array of the mtgCard struct named cards
     mtgCard[] public cards;
-    
+// @notice mapping a cardID to an address so we could know who owns a card.
     mapping (uint => address) public cardToOwner;
+// @notice mapping an address to the number of cards that address owns.
     mapping(address => uint) ownerCardCount;
-    
+
+// @notice Adds a card into the cards array
+// @param _name the name of the card
+// @param _cmc the converted mana cost of the card
+// @param _cardType the card type of the card
+// @param _colors the colors of the card
+// @param _price the price of the card. The price should be in wei
+// @param _image the hash image url of the card in IPFS
+// @dev We take the cardID, we assign a the initial card owner and we add 1 to his card count in this function.
     function addCardToInventory(string _name, string _cmc, string _cardType,string _colors, uint _price, string _image) public {
         uint id = cards.push(mtgCard(0,_name, _cmc, _cardType, _colors, _price, _image)) - 1;
         _addCardID(cards[id], id);
@@ -32,7 +41,13 @@ contract mtgInventory {
         ownerCardCount[msg.sender] = ownerCardCount[msg.sender].add(1);
         NewCardInventory(id, _name,_price);
     }
-    
+
+// @notice Purchases a card in the market
+// @param _id the id of the card to be purchased
+/* @dev We require that the person buying a card shouldn't be the owner of the card and that the msg.value 
+        being sent is equal to the card's price. After these conditions are met, we change the owner's address to the buyers, 
+        while sending the original owner half of the card's selling price. 
+        We then add the buyer's card count by 1 and reduce the sellers by 1*/
     function purchaseCard(uint _id) public payable {
         require(cardToOwner[_id]!= msg.sender);
         require(msg.value == cards[_id].price);
@@ -43,11 +58,19 @@ contract mtgInventory {
         ownerCardCount[msg.sender] = ownerCardCount[cardToOwner[_id]].add(1);
     }
     
+// @notice Changing a card in your inventory's price
+// @param _id the id of the card whose price you want to change
+// @param _price the new price of the card
+// @dev We require ownership of the card, then we change it if the condition is met.  
     function changeCardPrice(uint _id, uint _price) public {
         require(msg.sender == cardToOwner[_id]);
         cards[_id].price = _price;
     }
     
+// @notice Gets all cards owned by an address
+// @param _owner the address of the owner
+// @return we return an array of card Ids that the address owns
+// @dev We get all the cards an address owns, we then return an array including all those cards  
     function getCardsByOwner(address _owner) external view returns(uint[]) {
         uint[] memory result = new uint[](ownerCardCount[_owner]);
         uint counter = 0;
@@ -59,7 +82,10 @@ contract mtgInventory {
         }
         return result;
     }
-    //newly added 9/21
+    
+// @notice Gets all cards. Every single one of them.
+// @return every single card id in the cards array
+// @dev We get all the cards!
     function getAllCards() external view returns(uint[]) {
         uint[] memory result = new uint[](cards.length);
         uint counter = 0;
@@ -69,7 +95,11 @@ contract mtgInventory {
         }
         return result;
     }
-    //newly added 9/22
+    
+// @notice Changes a card's ID after adding it into the cards array
+// @param _card the card whose Id we will change
+// @param _id the id we want it to change into
+// @dev This was so that we can assign an Id to a specific card.
     function _addCardID(mtgCard storage _card, uint _id) internal {
         _card.id = _id;
     }
--- public/marketscript.js
+++ public/marketscript.js
@@ -4,49 +4,40 @@ $(document).ready(function () {
   var viewOn = viewInventory;
   var txTest = document.getElementById('txHere');
 
+  //Changes the view when clicking Home AKA Magic:The Ethering
   $('#linkHome').click(function () {
       viewOn = viewHome;
       showView("viewHome");
-      //alert("clickHome");
   });
-    
+  //Changes the view when clicking Inventory
   $('#linkInventory').click(function () {
       viewOn = viewInventory;
       showView("viewInventory");
-      //alert("clickInventory");
   });
-
+  //Changes the view when clicking Market
   $('#linkMarket').click(function () {
       viewOn = viewMarket;
       showView("viewMarket");
-      //alert("clickMark");
   });
-
-   $('#linkAbout').click(function () {
-      viewOn = viewAbout;
-      showView("viewAbout");
-      //alert("clickAbout");
-  });
-
+  //Handles View Changes
   function showView(viewName) {
       // Hide all views and show the selected view only
       $('main > section').hide();
       $('#' + viewName).show();
   }
-
+  //Handles the start of the app.
   function startApp() {
-      mtgMarketAddress = "0xe8d147d4241c1672313394f6b57d874cd2005280";
+      //The address of the deployed Smart Contract in the Ropsten Testnet
+      mtgMarketAddress = "0x70415d4efd87ebf8fdfd84aeb4d8c5fe9f6149d6";
       mtgMarket = new web3js.eth.Contract(mtgMarketABI, mtgMarketAddress);
 
       var account;
-      // // web3.eth.getAccounts(account);
-      // alert(account);
 
+      //checks for the metamask account every millisecond
       var accountInterval = setInterval(function() {
          web3.eth.getAccounts((err, res) => {                   
         
           account = res[0];
-          //console.log(account);
         });
           // Check if account has changed
           if (account !== userAccount) {
@@ -67,6 +58,7 @@ $(document).ready(function () {
       }, 100);
   }
 
+  //Displays the owned cards of the metamask address in the inventory view
   function displayCards(ids) {
     $("#txHere").empty();
       for (id of ids){ 
@@ -75,8 +67,8 @@ $(document).ready(function () {
             var priceEth = mtgCard.price / 1000000000000000000;
               $("#txHere").append(`<div class="responsive">
                 <div class="gallery">
-                  <a target="_blank" href="images/vraska.jpg">
-                    <img src="images/vraska.jpg" alt="Cinque Terre" width="600" height="400">
+                  <a target="_blank" href="https://ipfs.io/ipfs/${mtgCard.image}">
+                    <img src="https://ipfs.io/ipfs/${mtgCard.image}" alt="Insert Card Here" width="600" height="400">
                   </a>
                   <div class="desc">
                   ID: <span class="idd">${mtgCard.id}</span><br/>
@@ -85,11 +77,11 @@ $(document).ready(function () {
                   </div>
                 </div>
               </div>`);
-
           });
       }
   }
 
+  //Displays all the cards in the Market view
   function displayCardsMarket(ids) {
     $("#txHereMarket").empty();
       for (id of ids){ 
@@ -98,8 +90,8 @@ $(document).ready(function () {
             var priceEth = mtgCard.price / 1000000000000000000;
               $("#txHereMarket").append(`<div class="responsive">
                 <div class="gallery">
-                  <a target="_blank" href="images/vraska.jpg">
-                    <img src="images/vraska.jpg" alt="Cinque Terre" width="600" height="400">
+                  <a target="_blank" href="https://ipfs.io/ipfs/${mtgCard.image}">
+                    <img src="https://ipfs.io/ipfs/${mtgCard.image}" alt="Insert Card Here" width="600" height="400">
                   </a>
                   <div class="desc">
                   ID: <span class="idd">${mtgCard.id}</span><br/>
@@ -112,39 +104,82 @@ $(document).ready(function () {
       }
   }
 
+  //When clicking the add to inventory button, adds a card to the smart contract
   $('#buttonAddInvent').click(function () {
       var name = $("#cardName").val();
       var cmc = $("#cardCMC").val();
       var cardtype = $("#cardType").val();
       var colors = $("#cardColors").val();
       var price= $("#cardPrice").val();
+
       var image= "No Image Yet";
 
-      //alert(name + cmc + cardtype + colors + price + image);
+      //handles the uploading to ipfs
+      var input, file, fr, imagehash;
 
-      addCardToInventory(name,cmc,cardtype,colors,price,image);
-      clearAddInventory();
+      input = $("#uploadPhoto");
+
+      file = $("#uploadPhoto").get(0).files[0];
+      fr = new FileReader();
+      fr.onload = receivedText();
+      
+      function receivedText() {
+          fr = new FileReader();
+          fr.readAsBinaryString(file);
+          
+          //IPFS START
+          const repoPath = 'ipfs-1111' //+ Math.random()
+          const ipfs = new Ipfs({ repo: repoPath })
+
+          ipfs.on('ready', () => {
+              const files = [
+              {
+                  path: 'image.png',
+                  content: ipfs.types.Buffer.from(btoa(fr.result),"base64")
+              }
+              ]
+
+              ipfs.files.add(files, function (err, files) {
+                  let url = "https://ipfs.io/ipfs/"+files[0].hash;
+                  console.log("Storing file on IPFS using Javascript. HASH: https://ipfs.io/ipfs/"+files[0].hash);
+                  console.log("Other Link: https://raulikidj.club/examples/ipfs/ipfsimg.html?HASH="+files[0].hash);
+                  ipfsPath = files[0].hash
+                  ipfs.files.cat(ipfsPath, function (err, file) {
+                  if (err) {
+                      throw err
+                  }
+                  img = file.toString("base64");
+                  //assigns the ipfs hash to an image
+                  image = files[0].hash;
+
+                  console.log(image);
+                  //adds the card to the inventory
+                  addCardToInventory(name,cmc,cardtype,colors,price,image);
+                  //clears the textfields
+                  clearAddInventory();
+                  })
+              })
+          })
+      }
   });
 
+  //when you click the description box of a card in the market, you prompt to purchase the card
   $("#txHereMarket").delegate("div.desc", "click", function(){
-    //alert($(this).children('div.desc').text());
-    //var nameValue = $(this).find('#lower').attr('name');
     var purchaseID = $(this).children('span.idd').text();
-    //alert(purchaseID);
     purchaseCard(purchaseID);
   });
 
+  //when you click the description box of a card in your inventory, you prompt to change that card's price. Opens the change price modal
   $("#txHere").delegate("div.desc", "click", function(){
     var originalPrice = $(this).children('span.priceTx').text();
     var priceToWei = originalPrice * 1000000000000000000;
     var changeCardPriceID = $(this).children('span.idd').text();
-    // alert(priceToWei);
-    // alert(changeCardPrice);
     $('#cardChangePriceId').val(changeCardPriceID);
     $('#cardChangePrice').val(priceToWei);
     $('#changeCardPriceModal').modal('toggle');
   });
 
+  //Actually initalizes the changing price process
   $('#buttonChangePrice').click(function () {
     var changePrice = $("#cardChangePrice").val();
     var changeCardPriceID = $("#cardChangePriceId").val();
@@ -153,41 +188,43 @@ $(document).ready(function () {
     $('#changeCardPriceModal').modal('hide');
   });
 
+  //Adds a card to inventory!
   function addCardToInventory(name, cmc, cardType, colors, price, image) {
-    // This is going to take a while, so update the UI to let the user know
-    // the transaction has been sent
-    $("#txNotif").text("Adding card to blockchain, might take a while...");
+    // Notifying the user that it will take a while
+    $.notify("Adding card to blockchain, might take a while...", "info");
     // Send the tx to our contract:
     return mtgMarket.methods.addCardToInventory(name, cmc, cardType, colors, price, image)
     .send({ from: userAccount })
     .on("receipt", function(receipt) {
-      $("#txNotif").text("Successfully cast " + name + "!");
+      $.notify("Successfully cast " + name + "!", "success");
       // Transaction was accepted into the blockchain, let's redraw the UI
       getCardsByOwner(userAccount).then(displayCards);
       getAllCards().then(displayCardsMarket);
     })
     .on("error", function(error) {
       // Do something to alert the user their transaction has failed
-      $("#txNotif").text(error);
+      $.notify(error, "error");
     });
   }
 
   function changeCardPrice(id, price) {
-    $("#txNotif").text("Changing card price...");
+    // Notifying the user that it will take a while
+    $.notify("Changing card price, might take a while...", "info");
     return mtgMarket.methods.changeCardPrice(id, price)
     .send({ from: userAccount })
     .on("receipt", function(receipt) {
-      $("#txNotif").text("Changed card price!");
-      // Transaction was accepted into the blockchain, let's redraw the UI
+      // Success! show the user that the price of the card has successfully been changed
+      $.notify("Changed card price!", "success");
       getCardsByOwner(userAccount).then(displayCards);
       getAllCards().then(displayCardsMarket);
     })
     .on("error", function(error) {
       // Do something to alert the user their transaction has failed
-      $("#txNotif").text(error);
+      $.notify(error, "error");
     });
   }
 
+  //Clears input fields in the add inventory modal
   function clearAddInventory() {
     $("#cardName").val("");
     $("#cardCMC").val("");
@@ -197,69 +234,72 @@ $(document).ready(function () {
   }
 
   function purchaseCard(id) {
-    $("#txNotifMarket").text("Purchasing Card...");
+    // Notifying the user that it will take a while
+    $.notify("Purchasing Card, might take a while...", "info");
     var cardPrice;
     getCardDetails(id)
     .then(function(mtgCard) {
       return mtgMarket.methods.purchaseCard(id)
       .send({ from: userAccount, value: mtgCard.price })
       .on("receipt", function(receipt) {
-        $("#txNotif").text("Gadzooks! Successfully bought a card!");
+        // Success! show the user that they have successfully bought a card!
+        $.notify("Gadzooks! Successfully bought a card!", "success");
         getCardsByOwner(userAccount).then(displayCards);
       })
       .on("error", function(error) {
-        $("#txNotif").text(error);
+        // Do something to alert the user their transaction has failed
+        $.notify(error, "error");
       });
     });
   }
 
+  //Calling the getCardDetails function from the smart contract
   function getCardDetails(id) {
       return mtgMarket.methods.cards(id).call()
   }
-
+  //Calling the cardToOwner function from the smart contract
   function cardToOwner(id) {
       return mtgMarket.methods.cardToOwner(id).call()
   }
-
+  //Calling the getCardsByOwner function from the smart contract
   function getCardsByOwner(owner) {
       return mtgMarket.methods.getCardsByOwner(owner).call()
   }
-
+  //Calling the getAllCards function from the smart contract
   function getAllCards() {
       return mtgMarket.methods.getAllCards().call()
   }
 
+  //When the site first loads, this function triggers first
   window.addEventListener('load', function() {
-  showView("viewInventory");
+  //sets initial view to the Home page
+  showView("viewHome");
   var txWarning = document.getElementById('txHere');
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
       // Use Mist/MetaMask's provider
-      txHere.innerHTML = "yesMetamask";
+      $.notify("You have Metamask!", "info");
       web3js = new Web3(web3.currentProvider);
     } else {
       // Handle the case where the user doesn't have web3. Probably 
       // show them a message telling them to install Metamask in 
       // order to use our app.
-      txHere.innerHTML = "noMetamask";
+      $.notify("Install Metamask first!", "info");
     }
-
-    // Now you can start your app & access web3js freely:
+    // Starting the app and accessing web3
     startApp()
-
   })
 
-  window.pressed = function(){
-      var a = document.getElementById('uploadPhoto');
-      var label = document.getElementById('colorLabel')
-      if(a.value == "")
+  //Handles the displaying of what card photo the user has currently chosen.
+  $("#uploadPhoto").change(function() {
+    if($("#uploadPhoto").val() == "")
       {
-          label.innerHTML = "Choose file";
+          $("#imageLabel").val("No Photo Chosen");
       }
       else
       {
-          var theSplit = a.value.split('\\');
-          label.innerHTML = theSplit[theSplit.length-1];
+          var theSplit = $("#uploadPhoto").val().split('\\');
+          $("#imageLabel").text(theSplit[theSplit.length-1]);
       }
-  };
+  });
 });
